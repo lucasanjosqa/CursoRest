f042f293cbd6df4822b9800f7bbf2ec080ba19e3
Lucas <lucas_anjos14@hotmail.com>
2025-04-28 14:58:40 -0300

    Adicionando devoTrabalharComXML

---
+++ src/main/java/br/ce/wcaquino/rest/UserXMLTest.java
@@ -0,0 +1,32 @@
+package br.ce.wcaquino.rest;
+
+import org.junit.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class UserXMLTest {
+
+    @Test
+    public void devoTrabalharComXML(){
+        given()
+        .when()
+                .get("https://restapi.wcaquino.me/usersXML/3")
+        .then()
+                .statusCode(200)
+
+                .rootPath("user")
+                .body(".name", is("Ana Julia"))
+                .body(".@id", is("3"))
+
+                .rootPath("user.filhos")
+                .body(".name.size()", is(2))
+
+                .detachRootPath("filhos")
+                .body("filhos.name[0]", is("Zezinho"))
+                .body("filhos.name[1]", is("Luizinho"))
+
+                .appendRootPath("filhos")
+                .body(".name", hasItems("Zezinho", "Luizinho"))
+        ;
+    }
+}
